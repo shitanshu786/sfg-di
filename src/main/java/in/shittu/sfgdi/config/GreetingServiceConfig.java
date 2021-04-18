7e89124a56648fcfbe244bed83d24d97a05b3d47
Shitanshu(Shittu) <shitanshu.student.eee14@itbhu.ac.in>
2021-04-19 04:14:02 +0530

    spring application.properties

--- src/main/java/in/shittu/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/in/shittu/sfgdi/config/GreetingServiceConfig.java
@@ -7,7 +7,6 @@ import in.shittu.sfgdi.services.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
-@PropertySource("classpath:datasource.properties")
 @ImportResource("classpath:sfgdi-config.xml")
 @Configuration
 public class GreetingServiceConfig {
